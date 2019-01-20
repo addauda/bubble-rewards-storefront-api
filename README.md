a9a12acf2679e5148a7658a452a35f89d8c9cd7b
Ahmed Dauda <ahmed.dauda@uoit.net>
2019-01-19 22:23:44 -0500

    Initialized repo with serverless go example for get/post

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# bubble-rewards-storefront-api
+<!--
+title: TODO
+description: This example demonstrates how to setup a simple HTTP endpoint in Go.
+layout: Doc
+framework: v1
+platform: AWS
+language: Go
+authorLink: 'https://github.com/sebito91'
+authorName: 'Sebastian Borza'
+authorAvatar: 'https://avatars0.githubusercontent.com/u/3159454?v=4&s=140'
+-->
---
+++ world/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// Response is of type APIGatewayProxyResponse since we're leveraging the
+// AWS Lambda Proxy Request functionality (default behavior)
+//
+// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
+type Response events.APIGatewayProxyResponse
+
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (Response, error) {
+
+	// //Get the path parameter that was sent
+	// name := request.PathParameters["name"]
+
+	// //Generate message that want to be sent as body
+	// message := fmt.Sprintf(" { \"Message\" : \"Hello %s \" } ", name)
+
+	// //Returning response with AWS Lambda Proxy Response
+	// return events.APIGatewayProxyResponse{Body: message, StatusCode: 200}, nil
+
+	var buf bytes.Buffer
+
+	body, err := json.Marshal(map[string]interface{}{
+		"message": "Okay so your other function also executed successfully!",
+	})
+	if err != nil {
+		return Response{StatusCode: 404}, err
+	}
+	json.HTMLEscape(&buf, body)
+
+	resp := Response{
+		StatusCode:      200,
+		IsBase64Encoded: false,
+		Body:            buf.String(),
+		Headers: map[string]string{
+			"Content-Type":           "application/json",
+			"X-MyCompany-Func-Reply": "world-handler",
+		},
+	}
+
+	return resp, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
